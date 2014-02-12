4e8702579381675f10be9b6dfc4f45c0730261d7
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2014-02-12 09:04:42 +0000

    update from prod

--- OSS/typo3-plugins/ast_countdown/trunk/pi1/tx_astcountdown_pi1.js
+++ OSS/typo3-plugins/ast_countdown/trunk/pi1/tx_astcountdown_pi1.js
@@ -1,13 +1,12 @@
 // Countdown
-maxDigits = (vDayLen==3)?9:8;
+AnzS=''; for(i=0; i<9; i++) AnzS +=' ';
+Leer=''; for(i=0; i<9; i++) Leer +='0';
 
-AnzS=''; for(i=0; i<maxDigits; i++) AnzS +=' ';
-Leer=''; for(i=0; i<maxDigits; i++) Leer +='0';
-
+// Images
 n = new Array(10);
 for(i=0; i<10; i++) {
 	n[i] = new Image(vWidth,vHeight);
-	n[i].src = imgPath+'c'+i+'.gif';
+	n[i].src = imgPath+'c'+i+'.'+imgExt;
 }
 
 
@@ -17,13 +16,19 @@ function addZero(n) {
 	return str+n.toString();
 }
 
+function addZeros(n) {
+	str = '';
+	if (n<10) str += '0';
+	if (n<100) str += '0';
+	return str+n.toString();
+}
 
 function showNums(w) {
 	var AnzW=Math.floor(Math.abs(w));
 	var we=''+Leer+AnzW;
-	we = we.substring(we.length-maxDigits,we.length);
-	for(var i=maxDigits-1; i>=0; i-=1) {
-		if(AnzS.charAt(i)!= we.charAt(i)) document.images['b'+i].src = imgPath+'c'+we.charAt(i)+'.gif';
+	we = we.substring(we.length-9,we.length);
+	for(var i=9-1; i>=0; i-=1) {
+		if(AnzS.charAt(i)!= we.charAt(i)) document.images['b'+i].src = imgPath+'c'+we.charAt(i)+'.'+imgExt;
 	}
 	AnzS = we;
 }
@@ -35,43 +40,34 @@ function countDown() {
 	vInterval = 1001-(newDate.getTime()%1000);
 
 	if (vDiff<=0) {
-		vDays  = (vDayLen==3)?000:00;
+		vDays  = 00;
 		vHours = 00;
 		vMins  = 00;
 		vSecs  = 00;
 		return;
 	}
 
-
-	arrDig = new Array();
-
 	vSecs  = addZero(vDiff%60);
-	arrDig['s1'] = vSecs.substr(1,1);
-	arrDig['s0'] = vSecs.substr(0,1);
+	v8 = vSecs.substr(1,1);
+	v7 = vSecs.substr(0,1);
 
 	vDiff  = Math.floor(vDiff/60);
 	vMins  = addZero(vDiff%60);
-	arrDig['m1'] = vMins.substr(1,1);
-	arrDig['m0'] = vMins.substr(0,1);
+	v6     = vMins.substr(1,1);
+	v5     = vMins.substr(0,1);
 
 	vDiff  = Math.floor(vDiff/60);
 	vHours = addZero(vDiff%24);
-	arrDig['h1'] = vHours.substr(1,1);
-	arrDig['h0'] = vHours.substr(0,1);
+	v4     = vHours.substr(1,1);
+	v3     = vHours.substr(0,1);
 
 	vDiff  = Math.floor(vDiff/24);
-	vDays  = vDiff.toString();
+	vDays  = addZeros(vDiff);
+    v2     = vDays.substr(2,1);
+	v1     = vDays.substr(1,1);
+	v0     = vDays.substr(0,1);
 
-	if (vDayLen==3) {
-		arrDig['d2'] = vDays.substr(2,1);
-		arrDig['d1'] = vDays.substr(1,1);
-		arrDig['d0'] = vDays.substr(0,1);
-	} else {
-		arrDig['d1'] = vDays.substr(1,1);
-		arrDig['d0'] = vDays.substr(0,1);
-	}
-
-	newNum = (vDayLen==3)?''+arrDig['d0']+arrDig['d1']+arrDig['d2']+arrDig['h0']+arrDig['h1']+arrDig['m0']+arrDig['m1']+arrDig['s0']+arrDig['s1']:''+arrDig['d0']+arrDig['d1']+arrDig['h0']+arrDig['h1']+arrDig['m0']+arrDig['m1']+arrDig['s0']+arrDig['s1'];
+	newNum = ''+v0+v1+v2+v3+v4+v5+v6+v7+v8;
 	showNums(newNum);
 	setTimeout('countDown()',vInterval);
 }
