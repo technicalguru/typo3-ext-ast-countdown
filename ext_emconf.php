9bad47cadbf7001f90872a72b0819cf555bd02a8
Ralph Schuster <github@ralph-schuster.eu>
2014-02-12 12:52:57 +0000

    prod bugfix

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,37 +1,40 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "ast_countdown".
-#
-# Auto generated 09-11-2010 15:00
-#
+# Extension Manager/Repository config file for ext: "ast_countdown"
+# 
+# Auto generated 25-04-2006 20:13
+# 
 # Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
+# Only the data in the array - anything else is removed by next write
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'AST Countdown',
-	'description' => 'Dispalys a graphical countdown to a given date. Output format is (d)dd:hh:ii:ss. You can select if you want to show rather two or three digits for the days.',
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'Countdown',
+	'description' => 'Provides a graphical countdown (output format: d:h:i:s) to a specific date. The date can be set by the Constant Editor easily and is transfered to a Javascript date object, so start counting the month beginning at 0 for january and so on!',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.2.3',
-	'dependencies' => '',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
+	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 1,
+	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author' => 'Andre Steiling',
-	'author_email' => 'steiling@elemente.ms',
-	'author_company' => 'elemente websolutions',
+	'author_email' => 'steiling@pilotprojekt.com',
+	'author_company' => 'high StandArt, PILOTprojekt }',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
+	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:25:{s:12:"ext_icon.gif";s:4:"4d6e";s:17:"ext_localconf.php";s:4:"7151";s:14:"ext_tables.php";s:4:"e871";s:28:"ext_typoscript_constants.txt";s:4:"7550";s:24:"ext_typoscript_setup.txt";s:4:"3209";s:13:"locallang.xml";s:4:"836d";s:16:"locallang_db.xml";s:4:"bd1d";s:33:"pi1/class.tx_astcountdown_pi1.php";s:4:"d798";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"64ee";s:26:"pi1/tx_astcountdown_pi1.js";s:4:"b4c9";s:24:"pi1/static/editorcfg.txt";s:4:"ac05";s:17:"pi1/digits/c0.gif";s:4:"bdb3";s:17:"pi1/digits/c1.gif";s:4:"e4e7";s:17:"pi1/digits/c2.gif";s:4:"d8e2";s:17:"pi1/digits/c3.gif";s:4:"b625";s:17:"pi1/digits/c4.gif";s:4:"034e";s:17:"pi1/digits/c5.gif";s:4:"65bd";s:17:"pi1/digits/c6.gif";s:4:"daf3";s:17:"pi1/digits/c7.gif";s:4:"46bf";s:17:"pi1/digits/c8.gif";s:4:"2255";s:17:"pi1/digits/c9.gif";s:4:"686e";s:17:"pi1/digits/ct.gif";s:4:"8744";s:19:"doc/wizard_form.dat";s:4:"679e";s:20:"doc/wizard_form.html";s:4:"94e0";}',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.1.99',
@@ -41,7 +44,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:26:{s:12:"ext_icon.gif";s:4:"346a";s:17:"ext_localconf.php";s:4:"3799";s:14:"ext_tables.php";s:4:"9170";s:28:"ext_typoscript_constants.txt";s:4:"205a";s:24:"ext_typoscript_setup.txt";s:4:"1384";s:15:"flexform_ds.xml";s:4:"158a";s:13:"locallang.xml";s:4:"c990";s:16:"locallang_db.xml";s:4:"2867";s:17:"locallang_tca.xml";s:4:"31b9";s:14:"doc/manual.sxw";s:4:"d706";s:33:"pi1/class.tx_astcountdown_pi1.php";s:4:"3890";s:26:"pi1/tx_astcountdown_pi1.js";s:4:"21aa";s:17:"pi1/digits/c0.gif";s:4:"bdb3";s:17:"pi1/digits/c1.gif";s:4:"e4e7";s:17:"pi1/digits/c2.gif";s:4:"d8e2";s:17:"pi1/digits/c3.gif";s:4:"b625";s:17:"pi1/digits/c4.gif";s:4:"034e";s:17:"pi1/digits/c5.gif";s:4:"65bd";s:17:"pi1/digits/c6.gif";s:4:"daf3";s:17:"pi1/digits/c7.gif";s:4:"46bf";s:17:"pi1/digits/c8.gif";s:4:"2255";s:17:"pi1/digits/c9.gif";s:4:"686e";s:17:"pi1/digits/ct.gif";s:4:"8744";s:18:"pi1/digits/ctd.gif";s:4:"8744";s:18:"pi1/digits/cth.gif";s:4:"8744";s:18:"pi1/digits/ctm.gif";s:4:"8744";}',
 );
 
 ?>
